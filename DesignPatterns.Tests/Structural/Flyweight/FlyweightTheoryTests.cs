35ee342f602c25b3a683295514043ec4081c7f1f
bartvanhoey <bartvanhoey@hotmail.com>
2018-03-13 14:33:21 +0100

    flyweight theory tests

---
+++ DesignPatterns.Tests/Structural/Flyweight/FlyweightTheoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DesignPatterns.Structural.Flyweight.Theory;
+using Xunit;
+
+namespace DesignPatterns.Tests.Structural.Flyweight
+{
+    public class FlyweightTheoryTests
+    {
+        [Fact]
+        public void MethodName()
+        {
+            var extrinsicstate = 22;
+
+            var factory = new FlyweightFactory();
+
+            var fX = factory.GetFlyweight("X");
+            fX.Operation(--extrinsicstate);
+
+            var fY = factory.GetFlyweight("Y");
+            fY.Operation(--extrinsicstate);
+
+            var fZ = factory.GetFlyweight("Z");
+            fY.Operation(--extrinsicstate);
+
+            var fU = new UnsharedConcreteFlyweight();
+            fU.Operation(--extrinsicstate);
+
+
+        }
+    }
+}
